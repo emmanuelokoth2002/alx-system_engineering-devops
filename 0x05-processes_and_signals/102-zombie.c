9553a911f731eed01c27b860fb697598961d5dbe
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-28 07:01:41 -0700

    Task 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int infinite_while(void)
+{
+    while (1)
+    {
+        sleep(1);
+    }
+    return (0);
+}
+
+int main(void)
+{
+    pid_t pid;
+    int i;
+
+    for (i = 0; i < 5; i++)
+    {
+        pid = fork();
+
+        if (pid == 0)
+        {
+            exit(0);
+        }
+        else if (pid > 0)
+        {
+            printf("Zombie process created, PID: %d\n", pid);
+        }
+        else
+        {
+            fprintf(stderr, "Fork failed\n");
+            return (1);
+        }
+    }
+
+    infinite_while();
+
+    return (0);
+}

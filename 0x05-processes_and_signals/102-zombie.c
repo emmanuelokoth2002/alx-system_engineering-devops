54212ff0371f14b3d396c08ce1b780c54077230b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-04-30 20:02:34 -0700

    Updates

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,45 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
 /*
- * Write a C program that creates 5 zombie processes.
+ * main:Write a C program that creates 5 zombie processes.
+ * infinite_while: returns 0
+ *
  *
  */
 int infinite_while(void)
 {
-    while (1)
-    {
-        sleep(1);
-    }
-    return (0);
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
 }
-
 int main(void)
 {
-    pid_t pid;
-    int i;
+	pid_t pid;
+	int i;
 
-    for (i = 0; i < 5; i++)
-    {
-        pid = fork();
+	for (i = 0; i < 5; i++)
+	{
+		pid = fork();
 
-        if (pid == 0)
-        {
-            exit(0);
-        }
-        else if (pid > 0)
-        {
-            printf("Zombie process created, PID: %d\n", pid);
-        }
-        else
-        {
-            fprintf(stderr, "Fork failed\n");
-            return (1);
-        }
-    }
-
-    infinite_while();
-
-    return (0);
+		if (pid == 0)
+		{
+			exit(0);
+		}
+		else if (pid > 0)
+		{
+			printf("Zombie process created, PID: %d\n", pid);
+		}
+		else
+		{
+			fprintf(stderr, "Fork failed\n");
+			return (1);
+		}
+	}
+	infinite_while();
+	return (0);
 }

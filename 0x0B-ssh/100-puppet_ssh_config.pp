a48926cf1e63cdd74531b38cf92c9a5330ffaf76
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-06-25 22:17:51 -0700

    Updates

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,19 +1,11 @@
 #!/usr/bin/env bash
 # SSH configuration file so that you can connect to a server without typing a password.
-package { 'augeas-tools':
-  ensure => installed,
+file { '/etc/ssh/ssh_config':
+  ensure  => file,
+  content => "PasswordAuthentication no\nIdentityFile ~/.ssh/school\n",
 }
 
-augeas { 'Turn off passwd auth':
-  context => '/files/etc/ssh/sshd_config',
-  changes => [
-    'set #comment[.="PasswordAuthentication"]/../PasswordAuthentication no',
-  ],
-}
-
-augeas { 'Declare identity file':
-  context => '/files/etc/ssh/ssh_config',
-  changes => [
-    'set #comment[.="IdentityFile"]/../IdentityFile ~/.ssh/school',
-  ],
+service { 'ssh':
+  ensure => running,
+  enable => true,
 }

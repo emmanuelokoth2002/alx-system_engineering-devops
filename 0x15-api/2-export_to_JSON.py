ee64187ed6db0ce3fc137df6f08466dd5a651c0b
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-24 13:04:54 -0700

    Updates

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -4,78 +4,19 @@
 Module to fetch and export employee TODO list progress to JSON format.
 """
 
+import json
 import requests
 import sys
-import json
-
-def get_employee_data(employee_id):
-    """
-    Fetches employee data and TODO list progress based on the given employee ID.
-
-    Args:
-        employee_id (int): The employee ID.
-
-    Returns:
-        tuple: A tuple containing employee name and a list of completed tasks.
-    """
-    base_url = 'https://jsonplaceholder.typicode.com'
-    user_url = f'{base_url}/users/{employee_id}'
-    todos_url = f'{base_url}/todos?userId={employee_id}'
-
-    try:
-        # Fetch employee information
-        response = requests.get(user_url)
-        response.raise_for_status()
-        employee_data = response.json()
-        employee_name = employee_data['name']
-
-        # Fetch employee's TODO list
-        response = requests.get(todos_url)
-        response.raise_for_status()
-        todos_data = response.json()
-
-        # Extract completed tasks
-        completed_tasks = []
-        for todo in todos_data:
-            if todo['completed']:
-                completed_tasks.append({"task": todo['title'], "completed": True, "username": employee_name})
-            else:
-                completed_tasks.append({"task": todo['title'], "completed": False, "username": employee_name})
-
-        return employee_name, completed_tasks
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-    except KeyError:
-        print(f"Error: Employee with ID {employee_id} not found.")
-        sys.exit(1)
-    except ValueError:
-        print(f"Error: Invalid data received from the API.")
-        sys.exit(1)
-
-def export_to_json(employee_id, employee_name, completed_tasks):
-    """
-    Exports employee TODO list progress to JSON format.
-
-    Args:
-        employee_id (int): The employee ID.
-        employee_name (str): The name of the employee.
-        completed_tasks (list): List of completed tasks.
-
-    Returns:
-        None
-    """
-    data = {str(employee_id): completed_tasks}
-    filename = f"{employee_id}.json"
-    with open(filename, mode='w') as file:
-        json.dump(data, file, indent=4)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: ./export_to_JSON.py <employee_id>")
-        sys.exit(1)
-
-    employee_id = int(sys.argv[1])
-    employee_name, completed_tasks = get_employee_data(employee_id)
-    export_to_json(employee_id, employee_name, completed_tasks)
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    tasks = requests.get("{}users/{}/todos".format(url, user_id)).json()
+    data_dict = {
+                user_id: [{"task": task.get("title"),
+                           "completed": task.get("completed"),
+                           "username": user.get("username")} for task in tasks]
+            }
+    with open("{}.json".format(user_id), "w", newline="") as file:
+        json.dump(data_dict, file)

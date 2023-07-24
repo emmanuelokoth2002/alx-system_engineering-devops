e10729e55b726113a127b8d1eedc998e501b63ff
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-24 12:51:38 -0700

    Updates

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,49 +1,23 @@
 #!/usr/bin/python3
-'''Exports todo list to CSV for a given employee id'''
+
+"""employee data to CSV format"""
 
 import csv
 import requests
 import sys
 
-base_url = 'https://jsonplaceholder.typicode.com/'
 
+if __name__ == "__main__":
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get("{}users/{}".format(url, user_id)).json()
+    tasks = requests.get("{}users/{}/todos".format(url, user_id)).json()
+    data_rows = [
+        [sys.argv[1], user.get("username"), task.get("completed"),
+            task.get("title")] for task in tasks
+    ]
 
-def do_request():
-    '''Performs request'''
-    if len(sys.argv) < 2:
-        return print('USAGE:', __file__, '<employee id>')
-    eid = sys.argv[1]
-    try:
-        _eid = int(sys.argv[1])
-    except ValueError:
-        return print('Employee id must be an integer')
-
-    response = requests.get(base_url + 'users/' + eid)
-    if response.status_code == 404:
-        return print('User id not found')
-    elif response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    user = response.json()
-
-    response = requests.get(base_url + 'todos/')
-    if response.status_code != 200:
-        return print('Error: status_code:', response.status_code)
-    todos = response.json()
-
-    user_todos = [todo for todo in todos
-                  if todo.get('userId') == user.get('id')]
-
-    filename = '{}.csv'.format(user.get('id'))
-    with open(filename, mode='w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, delimiter=',', quotechar='"',
-                                quoting=csv.QUOTE_ALL)
-        for todo in user_todos:
-            csv_writer.writerow([
-                user.get('id'),
-                user.get('name'),
-                todo.get('completed'),
-                todo.get('title')
-            ])
-
-if __name__ == '__main__':
-    do_request()
+    with open("{}.csv".format(sys.argv[1]), "w", newline="") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        for data in data_rows:
+            writer.writerow(data)

c4455f7eb875bb76288f51537f4b87a3a66f0530
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-07-24 12:20:31 -0700

    Updates

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,14 +3,39 @@
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
-    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
-    c_tasks = [task for task in tasks if task.get("completed") is True]
-    titles = [task.get("title") for task in tasks
-              if task.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name.get("name"), len(c_tasks), len(tasks)))
-    for title in titles:
-        print("\t {}".format(title))
+base_url = 'https://jsonplaceholder.typicode.com/'
+
+
+def do_request():
+    '''Performs request'''
+    if len(sys.argv) < 2:
+        return print('USAGE:', __file__, '<employee id>')
+    eid = sys.argv[1]
+    try:
+        _eid = int(sys.argv[1])
+    except ValueError:
+        return print('Employee id must be an integer')
+
+    response = requests.get(base_url + 'users/' + eid)
+    if response.status_code == 404:
+        return print('User id not found')
+    elif response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    user = response.json()
+
+    response = requests.get(base_url + 'todos/')
+    if response.status_code != 200:
+        return print('Error: status_code:', response.status_code)
+    todos = response.json()
+
+    user_todos = [todo for todo in todos
+                  if todo.get('userId') == user.get('id')]
+    completed = [todo for todo in user_todos if todo.get('completed')]
+    print('Employee', user.get('name'),
+          'is done with tasks({}/{}):'.
+          format(len(completed), len(user_todos)))
+    [print('\t', todo.get('title')) for todo in completed]
+
+
+if __name__ == '__main__':
+    do_request()

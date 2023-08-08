0df9ef8b87988a62af06593168ff5ba532d0bccb
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-08-08 12:06:48 -0700

    Updates

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+Module to query the Reddit API for the number of subscribers in a subreddit.
+"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers for a given subreddit.
+    
+    Args:
+        subreddit (str): The name of the subreddit.
+        
+    Returns:
+        int: The number of subscribers for the subreddit. If subreddit is invalid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data["data"]["subscribers"]
+        return subscribers
+    else:
+        return 0
+
+if __name__ == "__main__":
+    import sys
+    
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit_name = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit_name)
+        print(subscribers)

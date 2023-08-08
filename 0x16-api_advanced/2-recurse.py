f2b860effa6f99c14ce04e2a0f46c92be2efc528
emmanuelokoth2002 <emmanuelokoth2002@gmail.com>
2023-08-08 12:23:09 -0700

    Updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,16 +6,17 @@ titles of all hot articles for a subreddit.
 
 import requests
 
+
 def recurse(subreddit, hot_list=[], after=None):
     """
     Recursively queries the Reddit API and returns a list containing the
     titles of all hot articles for a subreddit.
-    
+
     Args:
         subreddit (str): The name of the subreddit.
         hot_list (list): A list to store the titles of hot articles.
         after (str): The "after" parameter for pagination.
-        
+
     Returns:
         list: A list containing the titles of all hot articles. If no results
         are found, returns None.
@@ -23,22 +24,22 @@ def recurse(subreddit, hot_list=[], after=None):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {"User-Agent": "Custom User Agent"}
     params = {"limit": 100, "after": after}
-    
+
     response = requests.get(url, headers=headers, params=params)
-    
+
     if response.status_code == 200:
         data = response.json()
         posts = data["data"]["children"]
-        
+
         if len(posts) == 0:
             return hot_list
-        
+
         for post in posts:
             title = post["data"]["title"]
             hot_list.append(title)
-        
+
         after = data["data"]["after"]
-        
+
         if after is None:
             return hot_list
         else:
@@ -48,7 +49,7 @@ def recurse(subreddit, hot_list=[], after=None):
 
 if __name__ == "__main__":
     import sys
-    
+
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
